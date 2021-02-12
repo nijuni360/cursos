b66829dc5f221c42b0bd37b179dd46e4948d37af
nivaldo <junior.xbox360x@gmail.com>
2021-02-12 17:20:23 -0300

    exercicio 5

---
+++ aula01/ex5.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
+
+n = int(input('Digite o numero que você deseja saber a tabuada:\n'))
+tb = n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10
+print('A tabuada do numero {} é {}'.format(n, tb))
