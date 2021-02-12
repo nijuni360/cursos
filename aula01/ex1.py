1fbc859eae61855d63b971e3f1d68ad313667cdd
nivaldo <junior.xbox360x@gmail.com>
2021-02-12 16:40:45 -0300

    exercicio 1

--- aula01/ex1.py
+++ aula01/ex1.py
@@ -1,1 +1,6 @@
-print('Ola mundo')
+# Faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e antecessor
+
+n = int(input('Digite um numero\n'))
+ac = n-1
+sc = n+1
+print('O antecessor do numero {} é {}. E o seu sucessor é {} .'.format(n, ac, sc))
