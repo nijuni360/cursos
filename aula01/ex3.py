9a5cd2e831ec057e6d9e0491138c68d332f4fabc
nivaldo <junior.xbox360x@gmail.com>
2021-02-12 17:09:28 -0300

    exercicio 3

---
+++ aula01/ex3.py
@@ -0,0 +1,6 @@
+# Faça o calculo da média de duas notas
+
+nt1 = int(input('Qual sua nota de matematica?\n'))
+nt2 = int(input('Qual sua nota de portugues?\n'))
+rs = nt1+nt2//2
+print('A sua media foi {}'.format(rs))
