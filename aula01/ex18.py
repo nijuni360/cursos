ab698d32f060afdeec721fd8912097536c64e5a8
nivaldo <junior.xbox360x@gmail.com>
2021-02-12 20:50:21 -0300

    exercicio 18

---
+++ aula01/ex18.py
@@ -0,0 +1,4 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome
+
+cd = str(input('Qual seu nome completo?\n'))
+print('Seu nome tem Silva?\n{}'.format('Silva' in cd))
