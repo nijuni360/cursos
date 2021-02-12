b46749b8b5d194be1aa5a6145644e118d947fdc2
nivaldo <junior.xbox360x@gmail.com>
2021-02-12 20:42:00 -0300

    exercicio 16

---
+++ aula01/ex16.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia um numero de 0 à 9999 e mostre na tela cada um dos digitos separados
+
+num = int(input('Digite um numero qualquer de até 4 digitos:\n'))
+u = num//1 % 10
+d = num//10 % 10
+c = num//100 % 10
+m = num//1000 % 10
+print('Analisando o numero {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
