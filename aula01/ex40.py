78d03fd716bfaf32dd57e8592b889cc0ab524344
nivaldo <junior.xbox360x@gmail.com>
2021-02-13 14:50:39 -0300

    exercicio 40

---
+++ aula01/ex40.py
@@ -0,0 +1,5 @@
+# Crie um programa que mostre na tela todos os numeros pares que estão no intervalo de 1 a 50
+
+print('Ola, os numeros pares de 1 até 50 são:\n')
+for pares in range(2, 51, 2):
+    print(pares)
